b8b9bb9e883951b684e4a12a37a8bb3c00bedff3
gavin-rilee <you@example.com>
2021-06-15 19:02:08 -0400

    fixed final

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -36,7 +36,7 @@ const restricted = (req, res, next) => {
   */
 
 const only = (role_name) => (req, res, next) => {
-  if (req.decidedJwt.role_name !== role_name) {
+  if (req.decodedJwt.role_name !== role_name) {
     res.status(403).json({ message: "This is not for you" });
   } else {
     next();
